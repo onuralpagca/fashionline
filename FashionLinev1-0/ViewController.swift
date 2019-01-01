ac461874a4701d1eecc87720bd091fa608805053
Onuralp <onuralpagca@gmail.com>
2019-01-01 03:06:24 +0300

    resim upload yapıldı

--- FashionLinev1-0/ViewController.swift
+++ FashionLinev1-0/ViewController.swift
@@ -7,19 +7,99 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentText: UITextField!
+    
+    @IBOutlet weak var uploadButton: UIButton!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        //IMAGEVIEW TIKLANABİLİR YAPIYORUZ---
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        //IMAGEVIEW TIKLANABİLİR YAPIYORUZ----
+        
     }
 
+    @objc func chooseImage(){
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+        
+    }
     
     
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String){
+        
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    //Resim Upload butonuna tıklanıldığında
+    @IBAction func actionButtonClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            
+            
+            let uuid = UUID().uuidString
+            
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { (metadata, error) in
+                if error != nil{
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    
+                    imageReference.downloadURL{(url, error) in
+                    
+                    if error == nil{
+                        
+                        let imageUrl = url?.absoluteString
+                        print(imageUrl)
+                        
+                        
+                    }
+                    
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+
     
     
 
