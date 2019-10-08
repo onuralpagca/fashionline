b29279dee4076352aa6fd353f8e7a09be213a65c
Onuralp <onuralpagca@gmail.com>
2019-10-08 17:49:01 +0300

    Initial Commit

---
+++ FashionLinev1-0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FashionLinev1-0
+//
+//  Created by Onuralp on 8.10.2019.
+//  Copyright © 2019 AlohaInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
